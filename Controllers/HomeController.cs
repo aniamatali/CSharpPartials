e64d69374034fbd51a34fb613b0b46f817512b2c
Alvin Niamatali and Hansen Hansen <alvin@>
2017-10-09 11:09:54 -0700

    creates partial csharp

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendLetter.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public ActionResult Index()
+        {
+          return View();
+        }
+    }
+}
